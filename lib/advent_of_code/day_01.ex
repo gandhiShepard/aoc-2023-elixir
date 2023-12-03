3b1948fc81169c25937a7027a3ae0891c71fcdf6
Pul <pul@pulsit.de>
2023-12-03 10:18:09 +0100

    Day 1 Part 1 Pul

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,36 +1,17 @@
 defmodule AdventOfCode.Day01 do
-  @string_to_digit %{
-    "one" => 1,
-    "two" => 2,
-    "three" => 3,
-    "four" => 4,
-    "five" => 5,
-    "six" => 6,
-    "seven" => 7,
-    "eight" => 8,
-    "nine" => 9
-  }
+
+  @newline 10
+
   @spec part1(String.t) :: non_neg_integer()
-  def part1(input) do
-    input
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn x -> Regex.replace(~r/[a-zA-Z]/, x, "") end)
-    |> Enum.map(fn x -> "#{String.first(x)}#{String.last(x)}" end)
-    |> Enum.reduce(0, fn x, acc -> String.to_integer(x) + acc end)
-  end
+  def part1(input, first \\ nil, last \\ nil, sum \\ 0)
+  def part1(<<d, input::binary>>, nil, nil, sum) when d in ?0..?9, do: part1(input, d-?0, d-?0, sum)
+  def part1(<<d, input::binary>>, first, _last, sum) when d in ?0..?9, do: part1(input, first, d-?0, sum)
+  def part1(<<@newline, input::binary>>, first, last, sum), do: part1(input, nil, nil, sum + 10*first + last)
+  def part1(<<_, input::binary>>, first, last, sum), do: part1(input, first, last, sum)
+  def part1(<<>>, _, _, sum), do: sum
 
   @spec part2(String.t) :: non_neg_integer()
   def part2(input) do
-    input
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn x -> Regex.scan(~r/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/, x) end)
-    |> Enum.map(fn x -> "#{word_to_digit(List.first(x))}#{word_to_digit(List.last(x))}" end)
-    |> Enum.reduce(0, fn x, acc -> String.to_integer(x) + acc end)
   end
 
-  Enum.each(@string_to_digit, fn {k, v} ->
-    defp word_to_digit([_, unquote(k)]), do: unquote(v)
-  end)
-  defp word_to_digit([_, int]), do: int
 end
-
