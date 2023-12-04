a8d9225f8bf4c825cf6fa58da3d139a81efff35f
Pul <pul@pulsit.de>
2023-12-04 10:18:34 +0100

    Pul: Da2, part 2 solution (checked)

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -27,6 +27,28 @@ defmodule AdventOfCode.Day02 do
 
   defp game_id("Game " <> id), do: String.to_integer(id)
 
-  def part2(_args) do
+  def part2(input, sum_of_minimal_set_powers \\ 0)
+  def part2(<<>>, sum_of_minimal_set_powers), do: sum_of_minimal_set_powers
+  def part2(input, sum_of_minimal_set_powers) do
+    [game, input] = String.split(input, "\n", parts: 2)
+    part2(input, sum_of_minimal_set_powers + minimal_set_power(game))
+  end
+
+  defp minimal_set_power(game) do
+    [_game, sets] = String.split(game, ":")
+    sets = String.split(sets, ";")
+    Enum.reduce(sets, {1, 1, 1}, &grow_minimal_set/2)
+    |> then(fn {red, green, blue} -> red * green * blue end)
+  end
+
+  defp grow_minimal_set(set, minimal_set) do
+    counts_and_numbers = String.split(set, ",")
+    Enum.reduce(counts_and_numbers, minimal_set, fn  count_and_number, {red, green, blue} ->
+      case String.split(count_and_number, " ", trim: true) do
+        [count, "red"] -> {max(red, String.to_integer(count)), green, blue}
+        [count, "green"] -> {red, max(green, String.to_integer(count)), blue}
+        [count, "blue"] -> {red, green, max(blue, String.to_integer(count))}
+      end
+    end)
   end
 end
